0c700e2dcac6b7777ac9846f1f65f2d8e1752e05
Remon K <remonk@2019-MBPro-16.local>
2020-02-23 11:29:29 -0800

    Initial.

--- find_pair/src/main/python/find_pairs.py
+++ find_pair/src/main/python/find_pairs.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
+
 # This fails when we have too many recursive calls
 def find_pairs_recursively(array, target_sum):
+
     def check_next_elem(elem, rest):
         if len(rest) < 1:
             print("\tGot to end of array.")
@@ -17,26 +21,28 @@ def find_pairs_loop(array, target_sum):
     for (i, x) in enumerate(array):
         if (target_sum-x) in array[i:]:
             print("\tPair: {} and {}".format(x, target_sum-x))
-            break
+            return
+    print("\tGot to end of array.")
 
 
 # Driver to test
 if __name__ == "__main__":
-    arr = range(5, 500000000, 3)
-    target_sum = 3698923
+    arr = range(2, 5000, 8)
+    target_sum = 5004 + (8*21)
     import timeit
 
     print("Loop:")
-    loopTime = timeit.timeit(lambda: find_pairs_loop(arr, target_sum), number=1)
-    print("\tExecution Time: {0:.5f}".format(loopTime))
+    loopTime = timeit.timeit(lambda: find_pairs_loop(arr, target_sum), number=1) * 1000 * 1000
+    print("\tExecution Time: {0:.0f} MilliSeconds".format(loopTime))
     # Loop:
-    # Pair: 5 and 3698918
-    # Execution Time: 0.00001
+        # Pair: 1508994 and 49999994
+        # Execution Time: 81 MicroSeconds
+
 
     print()
     print("Recursive:")
-    recursiveTime = timeit.timeit(lambda: find_pairs_recursively(arr, target_sum), number=1)
-    print("\tExecution Time: {0:.5f}".format(recursiveTime))
+    recursiveTime = timeit.timeit(lambda: find_pairs_recursively(arr, target_sum), number=1) * 1000 * 1000
+    print("\tExecution Time: {0:.0f} MilliSeconds".format(recursiveTime))
     # Recursive:
     # Pair: 5 and 3698918
     # Execution Time: 0.00001
